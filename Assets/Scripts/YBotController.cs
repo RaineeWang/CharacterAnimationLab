feb0a37607d6a720a4ff6770c91e754188139473
RaineeWang <77654869+RaineeWang@users.noreply.github.com>
2021-11-08 22:40:04 +0400

    Blend Trees(not really working but done...)

--- Assets/Scripts/YBotController.cs
+++ Assets/Scripts/YBotController.cs
@@ -10,6 +10,8 @@ public class YBotController : MonoBehaviour
     NavMeshAgent agent;
 
     int isWalkingHash;
+    int velocityZHash;
+    int velocityXHash;
 
     private void Awake()
     {
@@ -17,12 +19,26 @@ public class YBotController : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
 
         isWalkingHash = Animator.StringToHash("isWalking");
+        velocityXHash = Animator.StringToHash("velocityX");
+        velocityZHash = Animator.StringToHash("velocityZ");
+
+
+        
     }
 
     private void Update()
     {
+        Vector3 velocity = agent.velocity;////the 2D Simple Directional, from blending tree
         bool isMoving = agent.velocity.magnitude > 0.01f && agent.remainingDistance > agent.radius;///once the remaining distance is witin the radius, we reached the target
 
         animator.SetBool("isWalking", isMoving);
+
+        ///transform the velocity, make the velocity LOCAL
+        velocity = transform.InverseTransformVector(velocity);
+        
+        animator.SetFloat("velocityXHash", velocity.x);
+        animator.SetFloat("velocityZHash", velocity.z);////if we jump, we will use velocity.y
+
+
     }
 }
